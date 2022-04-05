c5f9b12b2849d18cadea6fada6cb6992455f2889
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-04-04 17:24:58 -0700

    update

--- CMakeProject.h
+++ CMakeProject.h
@@ -4,5 +4,20 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <math.h>
+#include <algorithm>
+#include <optional>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <unordered_map>
+#include <map>
+#include <memory>
+#include <functional>
 
 // TODO: Reference additional headers your program requires here.
