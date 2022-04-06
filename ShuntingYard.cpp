1de9e3d2fe529382b4970129552116964f93c696
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-04-05 22:00:58 -0700

    update

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -1,23 +1,5 @@
 #include "ShuntingYard.h"
 
-struct Token {
-	enum struct Type {
-		Unknown,
-		Number,
-		Operator,
-		LeftParen,
-		RightParen,
-		Variable,
-	};
-
-	Token(Type t, const std::string& s, int prec = -1, bool ra = false) : type{ t }, str(s), precedence{ prec }, rightAssociative{ ra }	{}
-
-	const Type type;
-	const std::string str;
-	const int precedence;
-	const bool rightAssociative;
-};
-
 std::deque<Token> ParseExpression(std::string& string) {
 	std::deque<Token> tokens;
 
--- ShuntingYard.h
+++ ShuntingYard.h
@@ -5,7 +5,23 @@
 #include <vector>
 #include <map>
 
-struct Token;
+struct Token {
+	enum struct Type {
+		Unknown,
+		Number,
+		Operator,
+		LeftParen,
+		RightParen,
+		Variable,
+	};
+
+	Token(Type t, const std::string& s, int prec = -1, bool ra = false) : type{ t }, str(s), precedence{ prec }, rightAssociative{ ra }	{}
+
+	const Type type;
+	const std::string str;
+	const int precedence;
+	const bool rightAssociative;
+};
 
 std::deque<Token> ParseExpression(std::string& string);
 
