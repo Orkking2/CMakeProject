2617bb34774f6edcdb68d1eda61e437ead8b642b
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-04-09 20:04:57 -0700

    up

--- Deque.h
+++ Deque.h
@@ -1,24 +1,24 @@
 #pragma once
 
-#include <iostream>
 #include <assert.h>
+#include <memory>
 
-template <typename Type>
-struct Item {
-	Item(Type& val, Item* next = nullptr, Item* prev = nullptr) : val_(val), next_(next), prev_(prev) {}
-	Type val_;
-	Item* next_;
-	Item* prev_;
+template <typename _Ty>
+struct _Linked_item {
+	_Linked_item(_Ty& val, _Linked_item* next = nullptr, _Linked_item* prev = nullptr) 
+		: val_(val), next_(next), prev_(prev) {}
+	_Ty val_;
+	_Linked_item* next_, prev_;
 };
 
-template <typename Type>
-class Deque {
+template <typename _Ty>
+class _Linked_list {
 public:
-	using Item = Item<Type>;
-	Deque() : first_item_(nullptr), last_item_(nullptr) {}
-	Deque(Type& i) : first_item_(new_plus_assert(i, nullptr, nullptr)), last_item_(nullptr) {}
-	Deque(Deque& d) : first_item_(d.front_ptr()), last_item_(d.back_ptr()) {}
-	~Deque() {
+	using Item = _Linked_item<_Ty>;
+	_Linked_list() : first_item_(nullptr), last_item_(nullptr) {}
+	_Linked_list(_Ty i) : first_item_(new_plus_assert(i, nullptr, nullptr)), last_item_(nullptr) {}
+	_Linked_list(_Linked_list& d) : first_item_(d.front_ptr()), last_item_(d.back_ptr()) {}
+	~_Linked_list() {
 		while (first_item_ != nullptr) {
 			Item* cashe = first_item_;
 			first_item_ = first_item_->next_;
@@ -26,20 +26,23 @@ public:
 		}
 		while (last_item_ != nullptr) {
 			Item* cashe = last_item_;
-			last_item_ = last_item_->next_;
+			last_item_ = last_item_->prev_;
 			delete cashe;
 		}
 	}
 	Item* front_ptr() { return first_item_; }
 	Item* back_ptr () { return  last_item_; }
 
-	void emplace_front(Type& i) {
+	void emplace_front(_Ty& i) {
 		if (first_item_ == nullptr) {
-			first_item_ = new_plus_assert(i, last_item_, nullptr);
 			if (last_item_ != nullptr) {
 				Item* cashe = last_item_;
 				while (cashe->prev_ != nullptr) cashe = cashe->prev_;
+				first_item_ = new_plus_assert(i, cashe, nullptr);
 				cashe->prev_ = first_item_;
+			}
+			else {
+				first_item_ = new_plus_assert(i, nullptr, nullptr);
 			}
 		}
 		else {
@@ -47,13 +50,16 @@ public:
 			first_item_ = first_item_->prev_;
 		}
 	}
-	void emplace_back(Type& i) {
+	void emplace_back(_Ty& i) {
 		if (last_item_ == nullptr) {
-			last_item_ = new_plus_assert(i, nullptr, first_item_);
 			if (first_item_ != nullptr) {
 				Item* cashe = first_item_;
 				while (cashe->next_ != nullptr) cashe = cashe->next_;
+				last_item_ = new_plus_assert(i, nullptr, first_item_);
 				cashe->next_ = last_item_;
+			}
+			else {
+				last_item_ = new_plus_assert(i, nullptr, nullptr);
 			}
 		}
 		else {
@@ -61,65 +67,73 @@ public:
 			last_item_ = last_item_->next_;
 		}
 	}
-	void push_back (Type i) {  emplace_back(i); }
-	void push_front(Type i) { emplace_front(i); }
-	void push_array_front(Type* arr, Type* end) { for (Type* i = arr; i != end; i++) emplace_front(*i); }
-	void push_array_back (Type* arr, Type* end) { for (Type* i = arr; i != end; i++)  emplace_back(*i); }
-	Type front() {
+	void push_back (_Ty i) {  emplace_back(i); }
+	void push_front(_Ty i) { emplace_front(i); }
+	void push_array_front(_Ty* arr, _Ty* end) { for (_Ty* i = arr; i != end; i++) emplace_front(*i); }
+	void push_array_back (_Ty* arr, _Ty* end) { for (_Ty* i = arr; i != end; i++)  emplace_back(*i); }
+	_Ty front() {
 		if (last_item_ != nullptr && first_item_ == nullptr && last_item_->prev_ == nullptr) {
 			return last_item_->val_;
 		}
 		assert(pop_possible());
 		return first_item_->val_;
 	}
-	Type pop_front() {
+	_Ty pop_front() {
 		if (last_item_ != nullptr && first_item_ == nullptr && last_item_->prev_ == nullptr) {
-			Type out = last_item_->val_;
+			_Ty out = last_item_->val_;
 			last_item_ = nullptr;
 			return out;
 		}
 		assert(pop_possible());
 		Item* cashe = first_item_;
 		first_item_ = first_item_->next_;
-		Type out = cashe->val_;
+		_Ty out = cashe->val_;
 		delete cashe;
 		return out;
 	}
-	Type back() {
+	_Ty back() {
 		if (first_item_ != nullptr && last_item_ == nullptr && first_item_->next_ == nullptr) {
 			return first_item_->val_;
 		}
 		assert(pop_possible());
 		return last_item_->val_;
 	}
-	Type pop_back() {
+	_Ty pop_back() {
 		if (first_item_ != nullptr && last_item_ == nullptr && first_item_->next_ == nullptr) {
-			Type out = first_item_->val_;
+			_Ty out = first_item_->val_;
 			first_item_ = nullptr;
 			return out;
 		}
 		assert(pop_possible());
 		Item* cashe = last_item_;
 		last_item_ = last_item_->prev_;
-		Type out = cashe->val_;
+		_Ty out = cashe->val_;
 		delete cashe;
 		return out;
 	}
-	void operator = (const Deque& d) {
+	void operator = (const _Linked_list& d) {
+		while (first_item_ != nullptr) {
+			Item* cashe = first_item_;
+			first_item_ = first_item_->next_;
+			delete cashe;
+		}
+		while (last_item_ != nullptr) {
+			Item* cashe = last_item_;
+			last_item_ = last_item_->prev_;
+			delete cashe;
+		}
 		for (Item* i = d.back_ptr(); i != nullptr; i = i->prev_) {
 			push_back(i);
 		}
 	}
 private:
-	Item* new_plus_assert(Type& i, Item* next, Item* prev) {
+	Item* new_plus_assert(_Ty& i, Item* next, Item* prev) {
 		Item* ptr = new Item(i, next, prev);
 		assert(ptr);
 		return ptr;
 	}
 	bool pop_possible() {
-		if (first_item_ == nullptr && last_item_ == nullptr) {
-			return false;
-		}
+		if (first_item_ == nullptr && last_item_ == nullptr) return false;
 		else if (first_item_ == nullptr) {
 			Item* cashe = last_item_;
 			while (cashe->prev_ != nullptr) cashe = cashe->prev_;
@@ -132,6 +146,8 @@ private:
 		}
 		return true;
 	}
+
+	// 
 	Item* first_item_;
 	Item*  last_item_;
 };
