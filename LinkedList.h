8fdaf9b4f0fb4f7c3894eae3612028693eaae456
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-04-20 22:44:06 -0700

    up

--- LinkedList.h
+++ LinkedList.h
@@ -4,12 +4,16 @@
 #include <cstdarg>
 #include <memory>
 
+#ifndef NULL
+#define NULL 0
+#endif // ifndef NULL
+
 template <typename _Ty>
 class _Linked_item {
 private: 
 	using Item = _Linked_item<_Ty>;
 public:
-	Item(const _Ty& val, Item* next = nullptr, Item* prev = nullptr) : val_(val), next_(next), prev_(prev) {}
+	Item(const _Ty& val, Item* next = NULL, Item* prev = NULL) : val_(val), next_(next), prev_(prev) {}
 	
 	_Ty val_;
 	Item* next_;
@@ -21,11 +25,11 @@ class _Linked_list {
 private:
 	using Item = _Linked_item<_Ty>;
 public:
-	_Linked_list()                : first_item_(nullptr),                              last_item_(nullptr)      {}
-	_Linked_list(_Ty i)           : first_item_(new_plus_assert(i, nullptr, nullptr)), last_item_(nullptr)      {}
+	_Linked_list()                : first_item_(NULL),                                 last_item_(NULL)      {}
+	_Linked_list(_Ty i)           : first_item_(new_plus_assert(i, NULL, NULL)),       last_item_(NULL)      {}
 	_Linked_list(_Linked_list& d) : first_item_(d.front_ptr()),                        last_item_(d.back_ptr()) {}
 	// Ellipses are sussy
-	_Linked_list(int count, ...)  : first_item_(nullptr),                              last_item_(nullptr)      {
+	_Linked_list(int count, ...)  : first_item_(NULL),                                 last_item_(NULL)      {
 		std::va_list list;
 		va_start(list, count);
 		for (int i = 0; i < count; i++) push_back(va_arg(list, _Ty));
@@ -34,29 +38,27 @@ public:
 	}
 	~_Linked_list() {
 		link();
-		while (first_item_ != nullptr) {
+		while (first_item_) {
 			Item* cashe = first_item_;
 			first_item_ = first_item_->next_;
 			delete cashe;
 		}
-		if (last_item_ != nullptr) delete last_item_;
+		if (last_item_) delete last_item_;
 	}
 	Item* front_ptr() { link(); return first_item_; }
 	Item* back_ptr () { link(); return  last_item_; }
 
 	void emplace_front(_Ty& i) {
-		link(); 
-		if (first_item_ == nullptr) { first_item_ = new_plus_assert(i, nullptr, nullptr); link(); }
+		if (!first_item_) { first_item_ = new_plus_assert(i, NULL, NULL); link(); }
 		else {
-			first_item_->prev_ = new_plus_assert(i, first_item_, nullptr);
+			first_item_->prev_ = new_plus_assert(i, first_item_, NULL);
 			first_item_ = first_item_->prev_;
 		}
 	}
 	void emplace_back(_Ty& i) {
-		link(); 
-		if (last_item_ == nullptr) { last_item_ = new_plus_assert(i, nullptr, nullptr); link(); }
+		if (!last_item_) { last_item_ = new_plus_assert(i, NULL, NULL); link(); }
 		else {
-			last_item_->next_ = new_plus_assert(i, nullptr, last_item_);
+			last_item_->next_ = new_plus_assert(i, NULL, last_item_);
 			last_item_ = last_item_->next_;
 		}
 	}
@@ -73,12 +75,12 @@ public:
 	inline void push_array_back    (_Ty* arr, int  len) { for (int  i = 0;   i < len;  i++) push_back    (arr[i]); }
 
 	_Ty front() {
-		assert(not_nullptr());
+		assert(not_null());
 		link();
 		return first_item_->val_;
 	}
 	_Ty pop_front() {
-		assert(not_nullptr());
+		assert(not_null());
 		link();
 		Item* cashe = first_item_;
 		first_item_ = first_item_->next_;
@@ -87,12 +89,12 @@ public:
 		return out;
 	}
 	_Ty back() {
-		assert(not_nullptr());
+		assert(not_null());
 		link();
 		return last_item_->val_;
 	}
 	_Ty pop_back() {
-		assert(not_nullptr());
+		assert(not_null());
 		link();
 		Item* cashe = last_item_;
 		last_item_ = last_item_->prev_;
@@ -101,17 +103,14 @@ public:
 		return out;
 	}
 	void operator = (const _Linked_list& d) {
-		while (first_item_ != nullptr) {
+		link();
+		while (first_item_) {
 			Item* cashe = first_item_;
 			first_item_ = first_item_->next_;
 			delete cashe;
 		}
-		while (last_item_ != nullptr) {
-			Item* cashe = last_item_;
-			last_item_ = last_item_->prev_;
-			delete cashe;
-		}
-		for (Item* i = d.back_ptr(); i != nullptr; i = i->prev_) push_back(i);
+		if (last_item_) delete last_item_;
+		for (Item* i = d.back_ptr(); i; i = i->prev_) push_back(i);
 	}
 private:
 	Item* new_plus_assert(const _Ty& i, Item* next, Item* prev) {
@@ -119,35 +118,16 @@ private:
 		assert(ptr);
 		return ptr;
 	}
-	void link() {
-		if (first_item_ == nullptr && last_item_ == nullptr) {
-			return;
-		}
-		else if (first_item_ != nullptr && first_item_->next_ == nullptr && last_item_ != nullptr) {
-			for (Item* ptr = last_item_; ptr != nullptr; ptr = ptr->prev_) {
-				first_item_->next_ = ptr;
-			}
-			link();
-		}
-		else if (last_item_ != nullptr && last_item_->prev_ == nullptr && first_item_ != nullptr) {
-			for (Item* ptr = first_item_; ptr != nullptr; ptr = ptr->next_) {
-				last_item_->prev_ = ptr;
-			}
-		}
-		else if (first_item_ != nullptr && first_item_->next_ != nullptr && last_item_ == nullptr) {
-			for (Item* ptr = first_item_; ptr != nullptr; ptr = ptr->next_) {
-				last_item_ = ptr;
-			}
-		}
-		else if (last_item_ != nullptr && last_item_->prev_ != nullptr && first_item_ == nullptr) {
-			for (Item* ptr = first_item_; ptr != nullptr; ptr = ptr->next_) {
-				last_item_ = ptr;
-			}
-		}
-	}
-	bool not_nullptr() {
-		if (first_item_ == nullptr && last_item_ == nullptr) { return false; }
+	bool not_null() {
+		if (!first_item_ && !last_item_) { return false; }
 		else { return true; }
+	}
+	void link() {
+		if (!first_item_ && !last_item_) { return; }
+		else if (first_item_ && !(first_item_->next_) &&  last_item_ ) { for (Item* ptr = last_item_;  ptr; ptr = ptr->prev_) { first_item_->next_ = ptr; } link(); }
+		else if (last_item_  && !(last_item_ ->prev_) &&  first_item_) { for (Item* ptr = first_item_; ptr; ptr = ptr->next_) { last_item_ ->prev_ = ptr; } link(); }
+		else if (last_item_  &&   last_item_ ->prev_  && !first_item_) { for (Item* ptr = last_item_;  ptr; ptr = ptr->prev_) { first_item_ = ptr; } }
+		else if (first_item_ &&   first_item_->next_  && !last_item_ ) { for (Item* ptr = first_item_; ptr; ptr = ptr->next_) { last_item_  = ptr; } }
 	}
 
 	Item* first_item_;
