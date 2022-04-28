a8d75f0f519d2f329f8b65f95d681ad89a440a94
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-04-28 14:16:48 -0700

    up

--- Optional.h
+++ Optional.h
@@ -2,16 +2,22 @@
 #ifndef _CUSTOM_OPTIONAL_
 #define _CUSTOM_OPTIONAL_
 
-#include <assert.h>
+#include <string>
+
+#if !defined _assert_ && defined _STRING_
+#define _assert_(expr, msg) static_assert(expr, const_cast<char*> (std::string("") + msg + " | ON LINE: " + __LINE__ + " | IN FILE: " + __FILE__ + '\n').c_str())
+#elif !defined _assert_ // ^^^^ defined _STRING / !defined _STRING_ vvvv
+#error #include <string> bozo
+#endif // 
 
 template <typename _Ty>
 class _Optional {
+public:
 	_Optional() : obj_ptr(NULL) {}
 	_Optional(_Ty obj) { set_obj(obj); }
 	~_Optional() { delete obj_ptr; }
-
 	_Ty get_obj() {
-		assert(is_set());
+		_assert_(is_set(), "Optional not set");
 		return *obj_ptr;
 	}
 	void set_obj(_Ty obj) {
