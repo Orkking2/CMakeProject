0cc2d0804b8588f76255b635e20d1d9032bab546
Orkking2 <nicolas.bird.voneyben@gmail.com>
2022-05-05 10:13:47 -0700

    up

--- Optional.h
+++ Optional.h
@@ -5,36 +5,35 @@
 #include "Defines.h"
 #include "assert.h"
 
-#define _OPTIONAL_NOT_SET "Trying to access unset optional"
-
 _NSTD_BEGIN
 template <typename _Ty>
 class _Optional {
 public:
 	_Optional() : _obj_ptr(NULL) {}
-	_Optional(_Ty obj) { set_obj(obj); }
+	_Optional(_Ty obj) { set(obj); }
 	~_Optional() { delete _obj_ptr; }
-	_Ty get_obj() {
-		_NSTD_ASSERT(is_set(), _OPTIONAL_NOT_SET);
+	_Ty& get() {
+		_NSTD_ASSERT(is_set(), _not_set_err);
 		return *_obj_ptr;
 	}
-	void set_obj(_Ty obj) {
+	void set(_Ty obj) {
 		if (_obj_ptr) delete _obj_ptr;
 		_obj_ptr = new _Ty;
 		*_obj_ptr = obj;
 	}
 	_Ty pop() {
-		_NSTD_ASSERT(is_set(), _OPTIONAL_NOT_SET);
+		_NSTD_ASSERT(is_set(), _not_set_err);
 		_Ty cashe = *_obj_ptr
-		delete obj_ptr;
+		delete _obj_ptr;
 		_obj_ptr = NULL;
 		return cashe;
 	}
-	inline bool is_set() {
+	bool is_set() {
 		return static_cast<bool> (_obj_ptr);
 	}
 private:
 	_Ty* _obj_ptr;
+	static const char* _not_set_err = "Trying to access unset optional";
 };
 
 
--- assert.h
+++ assert.h
@@ -4,46 +4,39 @@
 #include "SmartObjects.h"
 _NSTD _S_ARR<char> assert_str(char* msg, char* expr, int line, char* file) {
 	// Standard strings
-	const char* err = "ERROR: ", * fr = " | FROM: ", * onl = " | ON LINE: ", * inf = " | IN FILE ";
-	const int err_len = 7, fr_len = 9, onl_len = 12, inf_len = 12;
+	static const char* err = "ERROR: ", * fr = " | FROM: ", * onl = " | ON LINE: ", * inf = " | IN FILE ";
+	static int err_len = _NSTD c_str_len(err), fr_len = _NSTD c_str_len(fr), onl_len = _NSTD c_str_len(onl), inf_len = _NSTD c_str_len(inf);
+	const int num_args = 8; // Must be manually updated
 
 	int msg_len = _NSTD c_str_len(msg), expr_len = _NSTD c_str_len(expr), line_len = 0, file_len = _NSTD c_str_len(file);
 	for (int cashe = line; cashe; cashe /= 10)
 		line_len++;
-	_NSTD _S_ARR<char> out(err_len + msg_len + fr_len + expr_len + onl_len + line_len + inf_len + file_len);
 	_NSTD _S_ARR<char> line_arr(line_len);
 
-	const char num_to_str_str[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	static const char num_to_str_str[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-	for(int i = 0, )
-
+	_NSTD_FOR(line_len) {
+		line_arr[i] = num_to_str_str[line % 10];
+		line /= 10;
+	}
 	char* c_l = line_arr.get_arr();
 
+	const char* str_args[num_args] = { err, msg, fr, expr, onl, c_l, inf, file };
+	const int   args_len[num_args] = { err_len, msg_len, fr_len, expr_len, onl_len, line_len, inf_len, file_len };
+
 	int cashe = 0;
-	_NSTD copy_to_out(out,  err,  err_len, cashe);
-	_NSTD copy_to_out(out,  msg,  msg_len, cashe);
-	_NSTD copy_to_out(out,   fr,   fr_len, cashe);
-	_NSTD copy_to_out(out, expr, expr_len, cashe);
-	_NSTD copy_to_out(out,  onl,  onl_len, cashe);
-	_NSTD copy_to_out(out,  c_l, line_len, cashe);
-	_NSTD copy_to_out(out,  inf,  inf_len, cashe);
-	_NSTD copy_to_out(out, file, file_len, cashe);
-
+	_NSTD _S_ARR<char> out(err_len + msg_len + fr_len + expr_len + onl_len + line_len + inf_len + file_len);
+	_NSTD_FOR(num_args)
+		_NSTD copy_to_out(out, str_args[i], args_len[i], cashe);
 
 	return out;
 }
-_NSTD_BEGIN
-int pow(int base, int power) {
-	if (power <= 0)
-		return 1;
-	int out = base;
-	for (int i = 0; i < power; i++)
-		out *= base;
-	return out;
-}
-int c_str_len(char* c_str) {
+
+_NSTD_BEGIN // Helper funcs
+
+int c_str_len(const char* c_str) {
 	int out = 0;
-	for (char* ptr = c_str; ptr != '\0'; ptr++)
+	for (char* ptr = const_cast<char*> (c_str); ptr != '\0'; ptr++)
 		out++;
 	return out;
 }
@@ -53,7 +46,13 @@ void copy_to_out(_NSTD _S_ARR<_Ty>& s_arr, const _Ty* arr, const int len, int& c
 		s_arr[cashe + i] = arr[i];
 	cashe += len;
 }
+int int_list_sum(int* arr, int len) {
+	int out = 0;
+	_NSTD_FOR(len)
+		out += arr[i];
+	return out;
+}
 _NSTD_END
-// "ERROR: " + msg + " | FROM: " + #expr + " | ON LINE: " + __LINE__ + " | IN FILE: " + __FILE__ ;
+// "ERROR: msg | FROM: #expr | ON LINE: __LINE__ | IN FILE: __FILE__ ";
 #define _NSTD_ASSERT(expr, msg) static_assert(!!(expr), assert_str(msg, #expr, __LINE__, __FILE__).get_arr())
 #endif // ifndef _NSTD_ASSERT
